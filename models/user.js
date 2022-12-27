94304b13d382158f58b1d5baa66458ee57970342
sangeeta-chaukulkar <sangeetachaukulkar05@gmail.com>
2022-12-27 21:18:02 +0530

    user model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+const ObjectId = mongodb.ObjectId;
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
-
+class User{
+  constructor(username,email){
+    this.name = username;
+    this.email = email
+  }
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this)
+  }
+  
+  static findUserById(userId){
+    const db = getDb();
+    return db.collection('users').findOne({_id : new ObjectId(userId)})
+    .then(user => {
+      console.log(user);
+      return user;
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+}
 module.exports = User;
