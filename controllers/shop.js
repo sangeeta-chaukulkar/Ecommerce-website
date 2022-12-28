a378478a331c2c94d03cba29f5c47269eae379ca
sangeeta-chaukulkar <sangeetachaukulkar05@gmail.com>
2022-12-28 15:13:12 +0530

    getting orders placed

--- controllers/shop.js
+++ controllers/shop.js
@@ -200,36 +200,22 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
+    .addOrder()
+        .then(result => {
+          res.redirect('/orders'); 
             })
-          );
-        })
         .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
-};
+}
+
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({include: ['products']})
+    .getOrders()
     .then(orders => {
-      res.json(orders);
-      // res.render('shop/orders', {
-      //   path: '/orders',
-      //   pageTitle: 'Your Orders',
-      //   orders: orders
-      // });
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
     })
     .catch(err => console.log(err));
 };
